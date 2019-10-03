712a66641b86aed458a022deada518deddd17796
Benoit Orihuela <benoit.orihuela@eglobalmark.com>
2019-10-03 06:56:54 +0200

    feat: #679 add options to indefinitely send new measures between a range

--- src/main/kotlin/com/egm/konem2m/commands/CiCommands.kt
+++ src/main/kotlin/com/egm/konem2m/commands/CiCommands.kt
@@ -8,19 +8,40 @@ import com.egm.konem2m.utils.generateRI
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.requireObject
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.double
+import com.github.ajalt.clikt.parameters.types.long
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 import com.google.gson.GsonBuilder
+import java.util.concurrent.ThreadLocalRandom
 
 class CiCreateCommands : CliktCommand(name = "ci-create") {
     private val origin by argument(help = "Originator of the request (prefixed with 'C' for CSEs or 'S' for AEs)")
     private val cntName by argument(help = "Name of the container in which to create the content instance")
-    private val value by argument(help = "Value to create")
+    private val value by argument(help = "Value to create").double()
+    private val repeatInterval: Long by option(help = "Time to wait before sending a new measure").long().default(-1)
+    private val minValue: Double by option(help = "Minimal value for a new measure").double().default(0.0)
+    private val maxValue: Double by option(help = "Maximal value for a new measure").double().default(100.0)
 
     private val config by requireObject<Map<String, String>>()
 
     override fun run() {
+
+        if (repeatInterval == -1L)
+            sendNewCi(value)
+        else {
+            while (true) {
+                val newValue = ThreadLocalRandom.current().nextDouble(minValue, maxValue)
+                sendNewCi(newValue)
+                Thread.sleep(repeatInterval)
+            }
+        }
+    }
+
+    private fun sendNewCi(value: Double) {
         val payload = """
             {
 	            "m2m:cin": {
@@ -47,7 +68,7 @@ class CiCreateCommands : CliktCommand(name = "ci-create") {
         when (result) {
             is Result.Success -> {
                 val ciLocation = response.header("Content-Location").first()
-                println("CI $ciLocation successfully created under $cntName".green())
+                println("CI $ciLocation with value $value created under $cntName".green())
             }
             is Result.Failure -> {
                 println(result.error.localizedMessage + " - " + response.body().asString("application/json").lightRed())
